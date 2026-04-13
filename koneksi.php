6b4685df50f914c859cf0122eab14740319d3c5b
indikartikaa <indi.kartikaa@gmail.com>
2026-04-13 16:22:43 +0700

    koneksi

--- koneksi.php
+++ koneksi.php
@@ -2,9 +2,8 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db   = "greasycle"; // Nama database yang kamu buat tadi
-
-$conn = mysqli_connect($host, $user, $pass, $db);
+$db   = "greasycle"; 
+$conn = mysqli_connect("192.168.110.115","root","", "greasycle");
 
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
